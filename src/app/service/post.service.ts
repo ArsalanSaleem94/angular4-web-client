6ce04990be9c542eebf77fe75eb933dd784782ba
Arsalan <arsalan@workstation-7559>
2017-08-24 05:03:20 -0400

    Reusable Error Handling Method

--- src/app/service/post.service.ts
+++ src/app/service/post.service.ts
@@ -26,27 +26,25 @@ export class PostService {
 
   createPost(post){
     return this.http.post(this.url, JSON.stringify(post))
-      .catch((error: Response) => {
-        if (error.status === 400){
-          return Observable.throw(new BadInput(error.json()));
-        }
-        else{
-          return Observable.throw(new AppError(error));
-        }
-        
-      });
+      .catch(this.handleError);
   }
 
   patchPost(){
-    return this.http.patch(this.url + "/1", JSON.stringify({isRead: true}));
+    return this.http.patch(this.url + "/1", JSON.stringify({isRead: true}))
+    .catch(this.handleError);
   }
 
   deletePost(id){
     return this.http.delete(this.url + '/' + id)
-      .catch((error: Response) => {
-        if(error.status === 404)
-            return Observable.throw(new NotFoundError());
-        return Observable.throw(new AppError(error));
-      });
+      .catch(this.handleError)
+  }
+
+  private handleError(error: Response){
+      if (error.status === 400){
+        return Observable.throw(new BadInput(error.json()));
+      }
+      if(error.status === 404)
+          return Observable.throw(new NotFoundError());
+      return Observable.throw(new AppError(error));
   }
 }
